4257ed1fd4faa74f81ecbb3c14e4ceab80fe4b98
SlejmUr <slejmur@protonmail.com>
2024-06-14 21:56:57 +0200

    ini change, trying to make gamesettings

---
+++ csharp/Menus/SettingsMenu.cs
@@ -0,0 +1,33 @@
+﻿#if CLIENT || GAME
+using ExtractIntoVoid.Managers;
+using Godot;
+
+public partial class SettingsMenu : Control
+{
+	public override void _Ready()
+	{
+		GameManager.Instance.UIManager.LoadScreenStop();
+	}
+
+
+	public void Save()
+	{
+
+	}
+
+    public void Apply()
+    {
+        this.GetWindow().Borderless = false;
+        this.GetWindow().Exclusive = false;
+        this.GetWindow().Size = new Vector2I(1920,2010);  
+    }
+
+    public void Back()
+    {
+
+    }
+
+    #region X
+    #endregion
+}
+#endif
